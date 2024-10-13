caaeb78af5f88720ad3dfad00137066bfe751ac7
mohammadfallaha <mohammadfallaha053@gmail.com>
2024-10-14 00:47:07 +0300

    Add project files.

---
+++ GameZone/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace GameZone.Controllers;
+public class HomeController : Controller
+{
+    private readonly IGamesService _gamesService;
+    private readonly ICategoriesService _categoriesService;
+    private readonly IDevicesService _devicesService;
+    public HomeController(IGamesService gamesService, ICategoriesService categoriesService, IDevicesService devicesService)
+    {
+        _gamesService = gamesService;
+        _categoriesService = categoriesService;
+        _devicesService = devicesService;
+    }
+
+    public IActionResult Index(string searchTerm = "", int? categoryId=null, int? deviceId = null)
+    {
+        ViewBag.Categories = _categoriesService.GetSelectList();
+        ViewBag.Devices = _devicesService.GetSelectList();
+        var games = _gamesService.GetAll(searchTerm,categoryId, deviceId);
+        return View(games);
+    }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+
+
+
+    public IActionResult Details(int id)
+    {
+        var game = _gamesService.GetById(id);
+
+        if (game is null)
+            return NotFound();
+
+        return View(game);
+    }
+}
---
+++ GameZone/Services/IGamesService.cs
@@ -0,0 +1,11 @@
+﻿namespace GameZone.Services;
+
+public interface IGamesService
+{
+    IEnumerable<Game> GetAll(string searchTerm = null, int? categoryId = null, int? deviceId = null);
+
+    Game? GetById(int id);
+    Task Create(CreateGameFormViewModel model);
+    Task<Game?> Update(EditGameFormViewModel model);
+    bool Delete(int id);
+}
